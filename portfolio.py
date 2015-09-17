f42811561e3389d63a590ae55eeb151babbfb7ee
kriordan <keith.riordan@gmail.com>
2015-09-16 23:32:59 -0400

    Add base template. Rename index template to home. Render home template.

--- portfolio.py
+++ portfolio.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Homepage'
+    return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
