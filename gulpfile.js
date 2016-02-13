6978f55342b4a6a2cfc783f9fd3e697bfb280322
kriordan <keith.riordan@gmail.com>
2016-02-12 23:36:01 -0500

    Add swp files to gitignore. Add less and browsersync to gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var less = require('gulp-less');
 
-gulp.task('default', function() {
+gulp.task('serve', ['less'], function() {
 
+    browserSync.init({
+        proxy: "localhost:5000"
+    });
+
+    gulp.watch("static/less/*.less", ['less']);
+    gulp.watch("templates/*.html").on("change", browserSync.reload);
 });
+
+gulp.task('less', function() {
+    return gulp.src("static/less/main.less")
+        .pipe(less())
+        .pipe(gulp.dest("static/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
