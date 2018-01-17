e1400029770dac4d7ceabb685290bc9fc63c8ddd
Keith Riordan <keith.riordan@gmail.com>
2018-01-16 22:56:17 -0500

    Add email contact form functionality. Add package-lock.json

--- portfolio.py
+++ portfolio.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template, request
 from forms import ContactForm
+from flask_mail import Message, Mail
+from datetime import datetime
 import yaml
 
+mail = Mail()
 app = Flask(__name__)
 
 app.secret_key = 'keyforflaskwtf'
 
 PROJECTS = yaml.load( open( 'yamlfiles/projects.yml' ) )
+
+# email configuration
+app.config["MAIL_SERVER"] = "smtp.sendgrid.net"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USERNAME"] = 'app42252860@heroku.com'
+app.config["MAIL_PASSWORD"] = '74oqyarq3851'
+
+mail.init_app(app)
+
+@app.context_processor
+def inject_now():
+  return {'now': datetime.utcnow()}
 
 @app.route('/')
 def home():
@@ -21,7 +36,14 @@ def resume():
 def contact():
     form = ContactForm()
     if form.validate_on_submit():
-        return 'Form posted.'
+        msg = Message(form.subject.data, sender='app42252860@heroku.com', recipients=['keith.riordan@gmail.com'])
+        msg.body = """
+        From: %s <%s>
+        %s
+        """ % (form.name.data, form.email.data, form.message.data)
+        mail.send(msg)
+        return render_template('contact.html', success=True)
+
     return render_template('contact.html', form=form)
 
 @app.route('/learningtospeak')
