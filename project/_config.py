790391acca6c82b40e2618d1371f04913563e078
Keith Riordan <keith.riordan@gmail.com>
2022-07-30 09:52:12 -0400

    Add log for debugging sqlalchemy error

--- project/_config.py
+++ project/_config.py
@@ -7,6 +7,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 db_uri = os.getenv("DATABASE_URL")
 if db_uri.startswith("postgres://"):
+    print("Replacing postgres URI scheme with postgresql")
     db_uri.replace("postgres://", "postgresql://", 1)
 SQLALCHEMY_DATABASE_URI = db_uri
 SQLALCHEMY_TRACK_MODIFICATIONS = True
