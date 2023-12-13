c7f8bb4328680d9955f1565e9d008bf552982fa8
Kriordan <keith.riordan@gmail.com>
2023-12-12 21:54:28 -0500

    Add title field to Gift. Update form

--- project/wishlist/forms.py
+++ project/wishlist/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
 
 class GiftForm(FlaskForm):
     body = TextAreaField("Body", validators=[DataRequired()])
+    title = StringField("Title", validators=[DataRequired()])
     submit = SubmitField("Submit")
