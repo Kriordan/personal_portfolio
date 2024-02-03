ede3749d1943f63779ae7a0577eae860622963e7
Kriordan <keith.riordan@gmail.com>
2024-02-03 06:15:44 -0500

    Add form macro to contact

--- project/foyer/forms.py
+++ project/foyer/forms.py
@@ -1,11 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import InputRequired, Email
+from wtforms import StringField, TextAreaField
+from wtforms.validators import Email, InputRequired
+
 
 class ContactForm(FlaskForm):
-  name = StringField("Name", validators=[InputRequired("Please enter your name.")])
-  email = StringField("Email", validators=[InputRequired("Please enter your email."), Email("Please enter a valid email.")])
-  subject = StringField("Subject", validators=[InputRequired("Please enter a subject.")])
-  message = TextAreaField("Message", validators=[InputRequired("Please enter a message.")])
-  submit = SubmitField("Send")
-
+    name = StringField("Name", validators=[InputRequired("Please enter your name.")])
+    email = StringField(
+        "Email",
+        validators=[
+            InputRequired("Please enter your email."),
+            Email("Please enter a valid email."),
+        ],
+    )
+    subject = StringField(
+        "Subject", validators=[InputRequired("Please enter a subject.")]
+    )
+    message = TextAreaField(
+        "Message", validators=[InputRequired("Please enter a message.")]
+    )
