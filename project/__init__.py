3666c26c733abada0dfeddca1111c3485f319588
Kriordan <keith.riordan@gmail.com>
2024-07-18 12:27:22 -0400

    Adjust csp

--- project/__init__.py
+++ project/__init__.py
@@ -39,7 +39,32 @@ app.config.from_pyfile("_config.py")
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-Talisman(app)
+csp = {
+    "default-src": ["'self'"],
+    "style-src": ["'self'", "https://fonts.googleapis.com", "'unsafe-inline'"],
+    "script-src": [
+        "'self'",
+        "https://www.googletagmanager.com",
+        "https://*.hotjar.com",
+        "https://cdn.jsdelivr.net",  # Add this line for AlpineJS CDN
+        "'unsafe-inline'",
+    ],
+    "img-src": [
+        "'self'",
+        f'https://{os.getenv("WISHLIST_S3_BUCKET")}.s3.amazonaws.com',
+    ],
+    "font-src": ["'self'", "https://fonts.gstatic.com"],
+    "connect-src": [
+        "'self'",
+        "https://*.google-analytics.com",
+        "https://stats.g.doubleclick.net",
+        "https://*.hotjar.com",
+        "https://*.hotjar.io",
+        "wss://*.hotjar.com",
+    ],
+}
+
+Talisman(app, content_security_policy=csp)
 
 if os.getenv("ENV") == "development":
     session = boto3.Session(profile_name="personalportfolio")
