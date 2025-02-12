68943d862781a89d9f1043bcecc94b440abbaccb
Keith Riordan <keith.riordan@gmail.com>
2025-02-11 21:51:24 -0500

    DB uri fix

--- project/_config.py
+++ project/_config.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 db_uri = os.getenv("DATABASE_URL")
 if db_uri.startswith("postgres://"):
-    db_uri = db_uri.replace("postgres://", "postgresql://", 1)
+    db_uri = db_uri.replace("postgres://", "postgresql+psycopg://", 1)
 SQLALCHEMY_DATABASE_URI = db_uri
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
